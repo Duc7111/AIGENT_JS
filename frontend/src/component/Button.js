91f18ef95be4b21cdf32b4c482762b255d3cf299
tringuyen@motive <95064327+tringuyenCoding@users.noreply.github.com>
2024-03-20 21:25:31 +0700

    header

---
+++ frontend/src/component/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Button(props) {
+    const {children, onClick, className} = props;
+    return (
+        <button onClick={onClick} className={className}>{children}</button>
+    )
+}
+
+export default Button;
